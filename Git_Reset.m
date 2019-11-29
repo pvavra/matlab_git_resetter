726c8bf5ac0d5904e1eb7507e56dcae18a86bf9e
Peter Vavra <peter.vavra.1984@gmail.com>
2019-11-29 16:33:48 +0100

    add simle git_reset() script

---
+++ Git_Reset.m
@@ -0,0 +1,17 @@
+function Git_Reset(useHardReset, removeUntracked)
+% Git_Reset(useHardReset) resets current folder to last commited version 
+% 
+% Use at the beginning of a script, to assert it is running the most recent
+% version
+
+if ~exist('removeUntracked','var')
+    removeUntracked = 1;
+end
+
+% reset all tracked files to last commit
+unix('git reset --hard HEAD')
+
+if removeUntracked
+    unix('git clean -f')
+end
+end
